77657a916084afcc3c5fc803010fd5883331a6bc
Rastislav Bertusek <Rastislav Bertusek>
2017-05-21 11:32:44 +0200

    inserting puzzle word input dummy component

--- client/app/modules/puzzle-game/puzzle-word/puzzle-word.controller.js
+++ client/app/modules/puzzle-game/puzzle-word/puzzle-word.controller.js
@@ -1,4 +1,13 @@
 class puzzleWordController {
+
+  onKeypress(event) {
+    if (!this.word || !this.word.selectedWord) { return; }
+    let testedSubWord  = this.word.correctWord + String.fromCharCode(event.keyCode);
+    if (this.word.selectedWord.indexOf(testedSubWord) !== 0) {
+      event.preventDefault();
+    }
+  }
+
   onNextWordClick() {
     this.handleNextWord({word: this.word});
   }
