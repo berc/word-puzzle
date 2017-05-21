8cdf5b54362eedca0d138753a317f23069cdbe6f
Rastislav Bertusek <Rastislav Bertusek>
2017-05-21 11:30:39 +0200

    event communication countdown dummy component

--- client/app/modules/puzzle-game/countdown/countdown.controller.js
+++ client/app/modules/puzzle-game/countdown/countdown.controller.js
@@ -1,27 +1,39 @@
 class countdownController {
   inst = null;
 
-  constructor($interval, Config) {
+  constructor($rootScope, $interval, Config) {
+    this.$rootScope = $rootScope;
     this.$interval = $interval;
     this.Config = Config;
   }
 
   $onInit() {
-    this.startCountdown();
+    this.registerOnStartGame();
   }
 
   startCountdown() {
+    if (this.inst) {
+      this.$interval.cancel(this.inst);
+    }
     this.countdown = this.Config.COUNTDOWN_SECONDS;
+
     this.inst = this.$interval(() => {
       this.countdown--;
 
-      if (this.countdown < 0) {
+      if (this.countdown <= 0) {
         this.countdown = 'Game over. :)';
         this.$interval.cancel(this.inst);
         this.inst = null;
+        this.$rootScope.$broadcast('gameOver');
       }
 
     }, 1000);
+  }
+
+  registerOnStartGame() {
+    this.$rootScope.$on('startGame', () => {
+      this.startCountdown();
+    })
   }
 
   $onDestroy() {
@@ -31,4 +43,4 @@ class countdownController {
   }
 }
 
-export default ['$interval', 'Config', countdownController];
+export default ['$rootScope', '$interval', 'Config', countdownController];
